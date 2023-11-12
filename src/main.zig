7e74d03aec2960a2e8010a2d471dc2d1877e11b6
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2023-11-12 19:51:57 +0100

    feat: Create rudimentary first render function

--- src/main.zig
+++ src/main.zig
@@ -1,21 +1,39 @@
 const std = @import("std");
 const testing = @import("std").testing;
 
-const cat =
-    \\ /|/|
+const cat = [_][]const u8{
+    \\ /|/|    
+    ,
     \\(. .) // 
-    \\ |##\//
-    \\ [w w]
-;
+    ,
+    \\ |##\//  
+    ,
+    \\ [w w]   
+};
 
 const box_width = 6;
-const cat_widht = 8;
+const cat_widht = 9;
+const expand_width = 0;
+
+const cat_height = 4;
+const box_height = 3;
+const expand_height = 0;
+
+const space_between = 5;
+
 // As string for less lines as code, more efficient if we do as int direct i think
 const default_columns = "100";
 
+// Prepare what we can at comptime
+// Copy the cat to it and
+comptime {
+    var renderBuffer: [cat_height + box_height + expand_height][]u8 = undefined;
+    _ = renderBuffer;
+}
+
 // I hate string manipulations
-pub fn create_box(allocator: std.mem.Allocator, name: *const []u8, box_wall_char: u8, box_floor_char: u8, box_space_char: u8) ![]const u8 {
-    const name_length = if (box_width > name.*.len) box_width else name.*.len;
+pub fn create_box(allocator: std.mem.Allocator, name: []const u8, box_wall_char: u8, box_floor_char: u8, box_space_char: u8) ![]const u8 {
+    const name_length = if (box_width > name.len) box_width else name.len;
     const line_lenght = 1 + name_length + 1 + 1;
     var buffer = try allocator.alloc(u8, line_lenght * 3);
 
@@ -28,12 +46,12 @@ pub fn create_box(allocator: std.mem.Allocator, name: *const []u8, box_wall_char
     }
 
     buffer[i] = box_wall_char;
-    buffer[i + 1] = '\n';
+    buffer[i + 1] = ' ';
 
     buffer[i + 2] = box_wall_char;
     i = i + 3;
 
-    const pad = name_length - name.*.len;
+    const pad = name_length - name.len;
     const pad_left = pad / 2;
     const pad_right = pad - pad_left;
 
@@ -42,7 +60,7 @@ pub fn create_box(allocator: std.mem.Allocator, name: *const []u8, box_wall_char
         i = i + 1;
     }
 
-    for (name.*) |character| {
+    for (name) |character| {
         buffer[i] = character;
         i = i + 1;
     }
@@ -53,7 +71,7 @@ pub fn create_box(allocator: std.mem.Allocator, name: *const []u8, box_wall_char
     }
 
     buffer[i] = box_wall_char;
-    buffer[i + 1] = '\n';
+    buffer[i + 1] = ' ';
 
     buffer[i + 2] = box_wall_char;
     i = i + 3;
@@ -64,9 +82,112 @@ pub fn create_box(allocator: std.mem.Allocator, name: *const []u8, box_wall_char
     }
 
     buffer[i] = box_wall_char;
-    buffer[i + 1] = '\n';
+    buffer[i + 1] = ' ';
 
     return buffer;
+}
+
+// Render a cat into the provided buffer
+fn renderCat(allocator: std.mem.Allocator, buffer: *[cat_height + box_height + expand_height][]const u8, colsize: usize, hostname: []const u8, username: []const u8) !void {
+
+    // Create space between spaces
+    var spaces: [space_between]u8 = undefined;
+    for (0..space_between) |i| {
+        spaces[i] = ' ';
+    }
+
+    // Line 0 is just the cat
+    buffer[0] = cat[0];
+
+    // Line 1 contains the username@hostname
+    var line1: []u8 = undefined;
+    line1 = try allocator.alloc(u8, 1 + cat_widht + space_between + hostname.len + 1 + username.len + 1);
+
+    std.mem.copy(u8, line1, cat[1]);
+    std.mem.copy(u8, line1[cat_widht..line1.len], &spaces);
+    line1[cat_widht + space_between] = ' ';
+    std.mem.copy(u8, line1[cat_widht + space_between + 1 .. line1.len], username);
+
+    line1[cat_widht + space_between + username.len + 1] = '@';
+    std.mem.copy(u8, line1[cat_widht + space_between + username.len + 1 + 1 .. line1.len], hostname);
+
+    line1[line1.len - 1] = ' ';
+
+    buffer[1] = line1;
+
+    // Line 2 contains the top of the second box
+
+    var boxlength: usize = 1 + hostname.len + 1 + username.len + 1;
+
+    if (boxlength < 5) {
+        boxlength = 5;
+    }
+
+    boxlength = boxlength + ((boxlength - 5) % 3);
+
+    var line2: []u8 = undefined;
+    line2 = try allocator.alloc(u8, cat_widht + space_between + boxlength);
+
+    std.mem.copy(u8, line2, cat[2]);
+    std.mem.copy(u8, line2[cat_widht..line2.len], &spaces);
+    line2[cat_widht + space_between] = '|';
+    for (cat_widht + space_between + 1..line2.len) |i| {
+        line2[i] = '=';
+    }
+
+    line2[line2.len - 1] = '|';
+    buffer[2] = line2;
+
+    //Line 3 Contains the circles
+
+    var line3: []u8 = undefined;
+    line3 = try allocator.alloc(u8, cat_widht + space_between + boxlength);
+    std.mem.copy(u8, line3, cat[3]);
+    std.mem.copy(u8, line3[cat_widht..line3.len], &spaces);
+    line3[cat_widht + space_between] = '|';
+
+    for (cat_widht + space_between + 1..line2.len - 1) |i| {
+        line3[i] = ' ';
+    }
+
+    line3[line2.len - 1] = '|';
+
+    buffer[3] = line3;
+
+    // TODO: This is very ugly redo
+    const box = try create_box(allocator, hostname, '|', '=', ' ');
+
+    const name_length = if (box_width > hostname.len) box_width else hostname.len;
+    const line_lenght = 1 + name_length + 2;
+
+    var line4: []u8 = undefined;
+    line4 = try allocator.alloc(u8, cat_widht + space_between + boxlength);
+
+    std.mem.copy(u8, line4, box[0..line_lenght]);
+    std.mem.copy(u8, line4[cat_widht..line4.len], &spaces);
+    line4[cat_widht + space_between] = '|';
+    for (cat_widht + space_between + 1..line4.len) |i| {
+        line4[i] = '=';
+    }
+
+    line4[line4.len - 1] = '|';
+
+    buffer[4] = line4;
+
+    var line5: []u8 = undefined;
+    line5 = try allocator.alloc(u8, line_lenght);
+
+    std.mem.copy(u8, line5, box[line_lenght .. line_lenght * 2]);
+
+    buffer[5] = line5;
+
+    var line6: []u8 = undefined;
+    line6 = try allocator.alloc(u8, line_lenght);
+
+    std.mem.copy(u8, line6, box[line_lenght * 2 .. line_lenght * 3]);
+    buffer[6] = line6;
+
+    _ = colsize;
 }
 
 fn output(lines: [][]const u8, file: std.fs.File) !void {
@@ -121,19 +242,18 @@ pub fn main() !void {
         std.os.exit(0);
     }
 
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    // Allocate the cat buffer
+    var content: [cat_height + box_height + expand_height][]u8 = undefined;
 
+    // Get the host name
     var hostname: [std.os.HOST_NAME_MAX]u8 = undefined;
     const name = try std.os.gethostname(&hostname);
 
-    const box = try create_box(box_alloc, &name, '|', '=', ' ');
-    defer box_alloc.free(box);
+    //Render the cat to the buffer
+    try renderCat(box_alloc, &content, cols, name, "Hi");
 
-    try stdout.print(cat, .{});
-    try stdout.print("\n", .{});
-    try stdout.print("{s}", .{box});
+    // Output the cat
+    const stdout_file = std.io.getStdOut();
 
-    try bw.flush(); // don't forget to flush!
+    try output(&content, stdout_file);
 }
